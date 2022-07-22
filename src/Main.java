ff6f9189b3380509b8de94150e82d9626de9917f
Alexey Panfilov <ap83@list.ru>
2022-07-22 15:18:20 +0300

    cutAverage method

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,12 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[] sales = {5, 13, 6, 8, 9};
+        int[] sales = {5, 13, 6, 8, 9}; // сумма 5 элементов = 41
         SalesManager manager = new SalesManager(sales);
 
         System.out.println(manager.max());
+
+        System.out.println(manager.cutAverage()); // 41 - 5 - 13 = 23; 23/3 = 7.7
+
     }
 }
